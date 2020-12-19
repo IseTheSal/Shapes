3152ce8582886d26c4a33384092a2cb24c2ae4d0
IseTheSal <illya.ageevich@gmail.com>
2020-12-19 22:16:47 +0300

    first commit

---
+++ test/by/learning/shape/parser/PyramidParserTest.java
@@ -0,0 +1,12 @@
+package by.learning.shape.parser;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PyramidParserTest {
+
+    @Test
+    public void testParsePyramid() {
+    }
+}
