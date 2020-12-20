711583f5adc9dcd03d1ab0c77a36f833b8e120df
IseTheSal <illya.ageevich@gmail.com>
2020-12-20 21:50:45 +0300

    first commit

---
+++ test/by/learning/shape/validator/FileValidatorTest.java
@@ -0,0 +1,12 @@
+package by.learning.shape.validator;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FileValidatorTest {
+
+    @Test
+    public void testIsFileValid() {
+    }
+}
